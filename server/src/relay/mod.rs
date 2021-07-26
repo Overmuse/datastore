2e2ce9ad9cba4e0e7e89dd9c868adffa73742219
Sebastian Rollen <seb@overmu.se>
2021-07-26 16:46:35 -0400

    make relay concurrent

--- server/src/relay/mod.rs
+++ server/src/relay/mod.rs
@@ -18,27 +18,31 @@ impl Relay {
 
     pub async fn run(&self) {
         tracing::info!("Starting relay");
-        let mut stream = self.consumer.stream();
-        while let Some(Ok(msg)) = stream.next().await {
-            if let Some(payload) = msg.payload() {
-                let maybe = serde_json::from_slice::<PolygonMessage>(payload);
-                if let Ok(PolygonMessage::Trade(trade)) = maybe {
-                    if trade.is_eligible() {
-                        trace!(ticker = %trade.symbol, price = %trade.price, "Trade");
-                        let key = format!("price/{}", trade.symbol);
-                        let _ = self.redis.set(&key, trade.price.to_f64().unwrap()).await;
-                    }
-                    if trade.is_opening() {
-                        debug!(ticker = %trade.symbol, price = %trade.price, "Open");
-                        let key = format!("open/{}", trade.symbol);
-                        let _ = self.redis.set(&key, trade.price.to_f64().unwrap()).await;
-                    } else if trade.is_closing() {
-                        debug!(ticker = %trade.symbol, price = %trade.price, "Close");
-                        let key = format!("close/{}", trade.symbol);
-                        let _ = self.redis.set(&key, trade.price.to_f64().unwrap()).await;
+        self.consumer
+            .stream()
+            .for_each_concurrent(100, |msg| async {
+                if let Ok(msg) = msg {
+                    if let Some(payload) = msg.payload() {
+                        let maybe = serde_json::from_slice::<PolygonMessage>(payload);
+                        if let Ok(PolygonMessage::Trade(trade)) = maybe {
+                            if trade.is_eligible() {
+                                trace!(ticker = %trade.symbol, price = %trade.price, "Trade");
+                                let key = format!("price/{}", trade.symbol);
+                                let _ = self.redis.set(&key, trade.price.to_f64().unwrap()).await;
+                            }
+                            if trade.is_opening() {
+                                debug!(ticker = %trade.symbol, price = %trade.price, "Open");
+                                let key = format!("open/{}", trade.symbol);
+                                let _ = self.redis.set(&key, trade.price.to_f64().unwrap()).await;
+                            } else if trade.is_closing() {
+                                debug!(ticker = %trade.symbol, price = %trade.price, "Close");
+                                let key = format!("close/{}", trade.symbol);
+                                let _ = self.redis.set(&key, trade.price.to_f64().unwrap()).await;
+                            }
+                        }
                     }
                 }
-            }
-        }
+            })
+            .await;
     }
 }
